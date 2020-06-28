c1457a0d4a31e832161077b7904dff391f02cf7d
roshansingha <roshansinghaagt@gmail.com>
2020-06-29 00:41:12 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
 
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition
 const mic = new SpeechRecognition()
 
 mic.continuous = true
@@ -13,15 +14,15 @@ function App() {
   const [note, setNote] = useState(null)
   const [savedNotes, setSavedNotes] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
     handleListen()
-  }, [isListening])
+  })
 
   const handleListen = () => {
-    if(isListening) {
+    if (isListening) {
       mic.start()
       mic.onend = () => {
-        console.log('continue...')
+        console.log('continue..')
         mic.start()
       }
     } else {
@@ -30,19 +31,20 @@ function App() {
         console.log('Stopped Mic on Click')
       }
     }
-    mic.onstart =() => {
-      console.log('Mics On')
+    mic.onstart = () => {
+      console.log('Mics on')
     }
 
     mic.onresult = event => {
-      const transcript = Array.from(event.results).map(result=>result[0])
-                              .map(result => result.transcript)
-                              .join('')
+      const transcript = Array.from(event.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
       console.log(transcript)
       setNote(transcript)
       mic.onerror = event => {
         console.log(event.error)
-      }                        
+      }
     }
   }
 
@@ -50,24 +52,31 @@ function App() {
     setSavedNotes([...savedNotes, note])
     setNote('')
   }
-  return(
-  <>
-    <h1> Voice Notes</h1>
-    <div className="container">
-      <div className="box">
-        <h2>Current Note</h2>
-        {isListening ? <span>🎙️</span> : <span>🛑🎙️</span>}
-        <button onClick={handleSaveNote} disabled={!note}>Save Note</button>
-        <button onClick={()=> setIsListening(prevState => !prevState)}>Start/Stop</button>
-        <p>{note}</p>
+
+  return (
+    <>
+      <h1>Voice Notes</h1>
+      <div className="container">
+        <div className="box">
+          <h2>Current Note</h2>
+          {isListening ? <span>🎙️</span> : <span>🛑🎙️</span>}
+          <button onClick={handleSaveNote} disabled={!note}>
+            Save Note
+          </button>
+          <button onClick={() => setIsListening(prevState => !prevState)}>
+            Start/Stop
+          </button>
+          <p>{note}</p>
+        </div>
+        <div className="box">
+          <h2>Notes</h2>
+          {savedNotes.map(n => (
+            <p key={n}>{n}</p>
+          ))}
+        </div>
       </div>
-      <div className="box">
-        <h2>Notes</h2>
-        {savedNotes.map(n =>(<p key={n}>{n}</p>))}
-      </div>
-    </div>
-  </>  
+    </>
   )
 }
 
-export default App;
+export default App

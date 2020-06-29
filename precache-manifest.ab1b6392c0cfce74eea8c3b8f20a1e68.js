3c9037b9e42029f5e650e788f29939916d0a662f
roshansingha <roshansinghaagt@gmail.com>
2020-06-29 11:29:47 +0530

    Updates

---
+++ precache-manifest.ab1b6392c0cfce74eea8c3b8f20a1e68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b60ae22dcd40f75c4e056a7ecfd6c8ad",
+    "url": "/SpeechtoText/index.html"
+  },
+  {
+    "revision": "995ff1a014fa2ed91bc0",
+    "url": "/SpeechtoText/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c2f9da634b1e5bb7787d",
+    "url": "/SpeechtoText/static/css/main.f0e463fa.chunk.css"
+  },
+  {
+    "revision": "995ff1a014fa2ed91bc0",
+    "url": "/SpeechtoText/static/js/2.1af64b43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpeechtoText/static/js/2.1af64b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f9da634b1e5bb7787d",
+    "url": "/SpeechtoText/static/js/main.4f346598.chunk.js"
+  },
+  {
+    "revision": "aed9c86d767af334137d",
+    "url": "/SpeechtoText/static/js/runtime-main.1345cbd6.js"
+  }
+]);
